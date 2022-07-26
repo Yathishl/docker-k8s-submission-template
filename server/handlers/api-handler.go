4f989e5ca0fcb59377df990c29b91b334cd53b5b
Yathishl <101790098+Yathishl@users.noreply.github.com>
2022-07-26 20:41:40 +0530

    Add files via upload

---
+++ server/handlers/api-handler.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yathishl/docker-k8s-submission-template/server/controllers"
+)
+
+type ApiHandler struct {
+	apiController controllers.ApiController
+}
+
+func NewApiHandler(apiController controllers.ApiController) *ApiHandler {
+	return &ApiHandler{
+		apiController: apiController,
+	}
+}
+
+func (h *ApiHandler) RootHandler(cxt *gin.Context) {
+	str := h.apiController.Root()
+
+	cxt.Data(http.StatusOK, "text/plain", []byte(str))
+	return
+}
+
+func (h *ApiHandler) CCGetHandler(cxt *gin.Context) {
+	str := h.apiController.CCGet()
+
+	cxt.Data(http.StatusOK, "text/plain", []byte(str))
+	return
+}
+
+func (h *ApiHandler) CCPostHandler(cxt *gin.Context) {
+	status, resp, err := h.apiController.CCPost(cxt)
+	if err != nil {
+		cxt.AbortWithError(status, err)
+		return
+	}
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		cxt.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	cxt.Writer.Write(respBody)
+	return
+}
---
+++ server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yathishl/docker-k8s-submission-template/server/controllers"
+	"github.com/yathishl/docker-k8s-submission-template/server/handlers"
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("API-Server running on port " + port)
+
+	server := gin.Default()
+
+	apiController := controllers.NewApiController()
+	apiHandler := handlers.NewApiHandler(apiController)
+
+	server.GET("/", apiHandler.RootHandler)
+	server.GET("/cc", apiHandler.CCGetHandler)
+	server.POST("/cc", apiHandler.CCPostHandler)
+
+	server.Run(":" + port)
+}
